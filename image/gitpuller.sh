5ec5a9794d5bcf8bf1c3b9fe7f6e7b0601047835
Stéphane Gully <gully@abes.fr>
2021-08-03 15:09:03 +0200

    refactor: indentation

--- image/gitpuller.sh
+++ image/gitpuller.sh
@@ -2,9 +2,9 @@
 
 # Git pull stuff
 if [ ! -d /folder-to-git-pull/.git/ ]; then
-       	echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Error: git pull not possible because /folder-to-git-pull/ is not a git clone result"
-	sleep 2 && exit 1
+  echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Error: git pull not possible because /folder-to-git-pull/ is not a git clone result"
+  sleep 2 && exit 1
 else
-	echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Update /folder-to-git-pull/ with a git pull"
-	git -C /folder-to-git-pull/ pull
+  echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Update /folder-to-git-pull/ with a git pull"
+  git -C /folder-to-git-pull/ pull
 fi
